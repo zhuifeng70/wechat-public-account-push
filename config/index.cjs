c2b86756fea6fe2c800f2afaff26004b78a5d1d3
zhuifeng70 <161805701+zhuifeng70@users.noreply.github.com>
2024-03-03 16:02:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -5,7 +5,58 @@
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
-const USER_CONFIG = {"APP_ID":"wx2a8742f8024a6abe","APP_SECRET":"c894836be858a98bcc2db6daaec22084","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"4men7c6LXm9w76ngOCgWvxoWZKAEqKb3X-uFR6MUyGI","CALLBACK_USERS":[{"name":"自己","id":"oEHVM68JMFmPtFtZ0pe0fyH41LUU"}],"USERS":[{"name":"郭亚萍女士","id":"oEHVM62Q6I3ht9kIvlpuJOt6KPJo","useTemplateId":"oOfOcDNUZNS4WT7hSCWaVjKD2ZU0NcgqKayf9pX-DYQ","province":"福建","city":"福州","horoscopeDate":"01-01","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"*生日","name":"美丽可爱的郭亚平女士","date":"11-20","year":"2024"}],"customizedDateList":[]}],"SWITCH":{"weather":true,"holidaytts":false,"CIBA":true,"oneTalk":false,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":true,"birthdayMessage":false}}
+const USER_CONFIG = {
+
+  // 使用微信测试号：公众号APP_ID
+  APP_ID: 'wx2a8742f8024a6abe',
+
+  // 使用微信测试号：公众号APP_SECRET
+  APP_SECRET: 'c894836be858a98bcc2db6daaec22084',
+
+  PROVINCE: '福建',
+  CITY: '福州',
+
+  USERS: [
+    {
+      // 想要发送的人的名字
+      name: '美丽可爱的郭亚平女士',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oEHVM62Q6I3ht9kIvlpuJOt6KPJo',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'oOfOcDNUZNS4WT7hSCWaVjKD2ZU0NcgqKayf9pX-DYQ',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '01-01',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '美丽可爱的郭亚平女士', year: '2024', date: '11-20',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+  ],
+
+
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  CALLBACK_TEMPLATE_ID: '4men7c6LXm9w76ngOCgWvxoWZKAEqKb3X-uFR6MUyGI',
+
+  CALLBACK_USERS: [
+    {
+      name: '自己',
+      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oEHVM68JMFmPtFtZ0pe0fyH41LUU',
+    }
+  ],
+
+}
+
+module.exports = USER_CONFIG
   
 
 
